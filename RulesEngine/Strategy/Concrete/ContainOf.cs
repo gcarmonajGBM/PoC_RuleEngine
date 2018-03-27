3cc6caaec3096a648f84985d3edca040faeb81c4
gcarmonaj <gcarmonaj@gbm.com.mx>
2018-03-26 18:14:46 -0600

    ContainsOf definition
    Evaluation for contains strategy

--- RulesEngine/Strategy/Concrete/ContainOf.cs
+++ RulesEngine/Strategy/Concrete/ContainOf.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using RulesEngine.Strategy.Abstract;
 
 namespace RulesEngine.Strategy.Concrete
@@ -14,6 +15,8 @@ namespace RulesEngine.Strategy.Concrete
         }
         public override bool Evaluate<T>(T contrastValue)
         {
+            var asQueryable = ((IEnumerable) contrastValue).AsQueryable();
+
             var type = contrastValue.GetType();
             
             return true;
